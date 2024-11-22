db356f974fe7cbd1ecc09c692559996402414ea0
Hyeongseok <a1932a@naver.com>
2024-11-22 16:33:20 +0900

    Feat: Login Page Layout 생성 (#4)

---
+++ app/src/main/java/com/provocation/checkmate/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.provocation.checkmate.presentation.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.provocation.checkmate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
